88477f2ce784385eeb6f908c37bf03dd0acc7f7d
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2024-06-03 20:11:58 +0300

    up

--- build/js/index.js
+++ build/js/index.js
@@ -4,3 +4,15 @@ AOS.init({
     once: true
 });
 
+// open Oferta
+
+const OfertaBtn = document.querySelectorAll('.oferta');
+const OfertaBody = document.querySelectorAll('.ofertaBody');
+
+OfertaBtn.forEach(item => {
+    item.addEventListener('click', () => {
+        OfertaBody.forEach(item => {
+            item.classList.toggle('h-[100%]')
+        })
+    })
+})
