571a3de4a1a5268aef663753ca2a863a0e42afe8
Volodymyr <61534586+VolodymyrBilichenko@users.noreply.github.com>
2024-06-05 10:43:04 +0300

    swiper

--- src/js/index.js
+++ src/js/index.js
@@ -16,3 +16,21 @@ OfertaBtn.forEach(item => {
         })
     })
 })
+
+
+// swiper
+
+const swiper = new Swiper(".mySwiper", {
+    slidesPerView: 1.2,
+    spaceBetween: 10,
+    pagination: {
+      el: ".swiper-pagination",
+      clickable: true,
+    },
+    breakpoints: {
+        992: {
+            slidesPerView: 4,
+            spaceBetween: 20,
+        }
+    }
+});
